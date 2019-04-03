a9869b8ed63eb25d43d69d0383d1affa35bb7354
Daniela Fernandez <daniela.fernandez7@gmail.com>
2019-04-03 19:46:56 -0300

    Juego finalizado

--- juego.rb
+++ juego.rb
@@ -1,6 +1,13 @@
 
 
-juego = ARGV[0]
+juego_mano = ARGV[0]
+if juego_mano == 0
+  juego = 'piedra'
+elsif juego_mano == 1
+  juego = 'papel'
+else
+  juego = 'tijera'
+end
 puts juego
 
 numero_comp = Random.new.rand(3)
@@ -16,4 +23,28 @@ puts mano
 
 if juego == 'piedra' and mano == 'papel'
   puts 'Perdiste'
-elsif 
+elsif
+  juego == 'piedra' and mano == 'piedra'
+  puts 'Empate'
+elsif
+  juego == 'piedra' and mano == 'tijera'
+  puts 'Ganaste'
+elsif
+  juego == 'papel' and mano == 'tijera'
+  puts 'Perdiste'
+elsif
+  juego == 'papel' and mano == 'papel'
+  puts 'Empate'
+elsif
+  juego == 'papel' and mano == 'piedra'
+  puts 'Ganaste'
+elsif
+  juego == 'tijera' and mano == 'papel'
+  puts 'Ganaste'
+elsif
+  juego == 'tijera' and mano == 'tijera'
+  puts 'Empate'
+else
+  juego == 'tijera' and mano == 'piedra'
+  puts 'Perdiste'
+end
